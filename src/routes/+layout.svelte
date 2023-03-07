067389e43c70afd5a951e88390957bdda6708999
Gabriel Wachowski <gabriel.wachowski@primacode.io>
2023-03-07 16:09:10 +0100

    implement simple calculator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,11 @@
 <script>
+	import Header from "$lib/components/Header.svelte";
 	import "../app.css";
-	import Header from './Header.svelte';
 </script>
 
 <div>
 	<Header />
-
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>Visit our <a href="https://github.com/wachuuu/how-much-gold">Github</a> and leave a star!</p>
-	</footer>
 </div>
