a63b37cf5c2ae495f7e197d2a2d075f4f2db7756
Gabriel Wachowski <gabriel.wachowski@primacode.io>
2023-03-13 13:38:00 +0100

    setup tailwind configuration

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,19 +1,3 @@
-<script>
-	import github from "$lib/images/github.svg";
-	import logo from "$lib/images/svelte-logo.svg";
-</script>
-
 <header>
-	<a href="https://kit.svelte.dev">
-		<img width="50px" src={logo} alt="SvelteKit" />
-	</a>
-
-	<nav>
-		<a href="/">Home</a>
-		<a href="/about">About</a>
-	</nav>
-
-	<a href="https://github.com/wachuuu/how-much-gold">
-		<img width="50px" src={github} alt="GitHub" />
-	</a>
+howmuch.<strong>gold</strong>
 </header>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,73 @@
+const plugin = require('tailwindcss/plugin')
+
+/** Base application colors */
+const appColors = {
+  primary: '#160B14',
+  secondary: '#FFFBFB',
+  highlight1: 'rgba(255, 255, 255, 0.07)',
+  highlight2: 'rgba(255, 255, 255, 0.15)',
+  positive: '#70FF00',
+  negative: '#FF0000',
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      ...appColors
+    },
+    extend: {
+      spacing: {
+        'input': '.25rem 1rem',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.secondary'),
+            '--tw-prose-headings': theme('colors.secondary'),
+            '--tw-prose-links': theme('colors.secondary'),
+            '--tw-prose-bold': theme('colors.secondary'),
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    plugin(function ({ addBase, addComponents, addUtilities, theme }) {
+      addBase({
+        'html': { background: theme('colors.primary') },
+        'input': {
+          'color': theme('colors.secondary'),
+          'background': theme('colors.highlight2'),
+          'border-radius': theme('borderRadius.full'),
+          'padding': theme('spacing.input'),
+          'backdrop-filter': 'blur(65px)',
+        },
+      }),
+      addComponents({
+        '.card': {
+          'color': theme('colors.dark'),
+          'background': theme('colors.highlight1'),
+          'border-radius': theme('borderRadius.2xl'),
+          'backdrop-filter': 'blur(65px)',
+        }
+      }),
+      addUtilities({
+        '.gradient-mobile': {
+          'background': `radial-gradient(98.21% 20.97% at 8.72% 24.11%, rgba(255,0, 0, 0.2) 30.21%, rgba(0, 0, 0, 0) 100%),
+          radial-gradient(84.12% 84.12% at 90.64% 15.88%, rgba(51, 119, 253, 0.2) 0%, rgba(0, 0, 0, 0) 100%),
+          radial-gradient(149.1% 31.84% at -16.79% 51.72%, rgba(211, 111, 195, 0.2) 0%, rgba(191, 58, 154, 0) 100%),
+          #160B14;`,
+        },
+        '.gradient-desktop': {
+          'background': `radial-gradient(74.69% 51.52% at 80.62% 35.06%, rgba(255, 0, 0, 0.2) 15.1%, rgba(0, 0, 0, 0) 100%),
+          radial-gradient(84.12% 84.12% at 90.64% 15.88%, rgba(51, 119, 253, 0.2) 0%, rgba(0, 0, 0, 0) 100%),
+          radial-gradient(149.1% 31.84% at -16.79% 51.72%, rgba(211, 111, 195, 0.2) 0%, rgba(191, 58, 154, 0) 100%),
+          #160B14;`,
+        }
+      })
+    }),
+  ],
 }
