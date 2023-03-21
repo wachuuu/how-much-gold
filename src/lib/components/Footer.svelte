daecf64b38a806f50f55cfd52c3e52ffb0c6aa69
Gabriel Wachowski <gabriel.wachowski@primacode.io>
2023-03-21 17:02:43 +0100

    style static elements

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import github from '$lib/images/github.svg';
+  import svelte from '$lib/images/svelte-logo.svg';
+
+  const link = 'https://github.com/wachuuu/how-much-gold';
+</script>
+
+<div class="not-prose flex align-center justify-center text-sm opacity-50">
+  Created with 
+  <img class="mx-1" src="{svelte}" width="16px" alt="svelte-logo">
+  (and love) by primacode.io. 
+  <a class="flex align-center mx-1 underline" href="{link}">
+    <img class="mr-1" src="{github}" width="16px" alt="github-logo">
+    Leave a star!
+  </a>
+</div>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,3 @@
-<header>
+<header class="flex justify-center my-4 text-xl md:my-20 md:text-6xl">
 howmuch.<strong>gold</strong>
 </header>
